a28814f0449094f5f7b1a5cc223172db6f8650c6
Sumit Jaiswal <sumitcv@gmail.com>
2023-11-23 00:24:46 +0530

    Update even_odd.c
    commit code

--- even_odd.c
+++ even_odd.c
@@ -1,1 +1,7 @@
-bitwise
+i & 1 == 0:
+If the result is true (1), it means i is even.
+If the result is false (0), it means i is odd.
+
+i & 1 == 1:
+If the result is true (1), it means i is odd.
+If the result is false (0), it means i is even.
